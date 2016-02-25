e3b7598f8fb5e0ff135d27fc62f17b042cdd15fe
Iouri Belokopytov <yb@nordita.org>
2016-02-25 12:27:23 +0000

    VM fixes

--- caa/myPear/myIndico/example.php
+++ caa/myPear/myIndico/example.php
@@ -4,17 +4,77 @@
  * <myPear root>/myIndico/agenda.inc
  */
 
+function array_sort($array, $on, $order=SORT_ASC) {
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on)           $sortable_array[$k] = $v2;
+                }
+            } else    $sortable_array[$k] = $v;
+        }
+
+        switch ($order) {
+        case SORT_ASC:
+            asort($sortable_array);
+            break;
+        case SORT_DESC:
+            arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
+
 require_once './agenda.inc';
 
 $agenda = new agenda('agenda.albanova.se');
 $agenda->debug = True;
 
 // Print a short "how to"
-print $agenda->help();
+// print $agenda->help();
 
 // Show conferences in category 315
-$reply = $agenda->getCategory(315);
+if (0) $reply = $agenda->getCategory(270,
+                                     join('&',array("after=2015-08-01",
+                                                    "before=2016-09-01",
+                                                    "show_reg=all",
+                                                    "show_contrib=all")));
+$reply = $agenda->getConference(4462,
+                                join('&',array(
+                                               "show_reg=all",
+                                               "show_contrib=all")));
+
+krsort($reply);
+foreach($reply as $date=>$data1){
+    foreach($data1 as $key=>$data2){
+        if (!is_array($data2)) continue;
+        switch ($key) {
+        case 'chairs':
+            // print_r($data2);                                                                                                                                                                                        
+            $reply[$date][$key] = array_sort($data2, 'av_lastname', SORT_ASC);
+            //      print_r($data1[$key]);                                                                                                                                                                             
+            break;
+        case 'registrant':
+            $reply[$date][$key] = array_sort($data2, 'familyname', SORT_ASC);
+            break;
+        case 'session':
+            break;
+        case 'chair':
+            break;
+        }
+    }
+}
 print_r($reply);
+exit;
 
 // Show conferences in category 315 with dates restricted
 $reply = $agenda->getCategory(315,"after=2015-08-01");
--- caa/vm/templates/test_av.php
+++ caa/vm/templates/test_av.php
@@ -1,13 +1,18 @@
 <?php
-//$_GET['showL'] = 1; $_GET['showl'] = 1;
+$_GET['debug'] = $_GET['dev'] = 1;
+$_GET['showL'] = 1; $_GET['showl'] = 1;
+$_GET['org'] = 'nordita';
 require_once '../myPear/config.inc';
 require_once '../vm/config.inc';
 locateAndInclude('pdf_form');
 
 #$v = myPear::getInstance('bForm_vm_Visit',45878,'fatal');
 #$v = myPear::getInstance('bForm_vm_Visit',45654,'fatal');
-$v = myPear::getInstance('bForm_vm_Visit',45422,'fatal');
+$v = myPear::getInstance('bForm_vm_Visit',45422);
+var_dump($v);
+die("???\n");
 $exp = $v->getExp();
+print_r($exp);
 
 switch(True){
 case True:
@@ -35,9 +40,9 @@ default:
 	$fdf[$a['t']] = b_fmt::unEscape(@$formDB[$ref]);
       }
     }
-  b_debug::print_r($fdf);
-  print pdf_form::fill($fdf,vm_root."templates/$tpl.pdf");//,"/tmp/$tpl.pdf");
-  print "\n";
+    b_debug::print_r($fdf);
+    print pdf_form::fill($fdf,vm_root."templates/$tpl.pdf");//,"/tmp/$tpl.pdf");
+    print "\n";
   }
   print "\n";
 }
